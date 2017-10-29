048d77cb9f6b01eb46a48536c718236bb774d37b
nashbridges <pleasesendmeaspam@gmail.com>
2017-10-29 10:42:45 +0200

    Update seeds; use factory_girl there

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
-Book.create!(title: "Зубр шукає гніздо")
-Book.create!(title: "Ведмідь не хоче спати")
+require "factory_girl_rails"
+
+text_author_type = FactoryGirl.create(:text_author_type)
+illustrator_type = FactoryGirl.create(:illustrator_type)
+
+oksana_bula = FactoryGirl.create(:person, :with_alias, first_name: "Оксана", last_name: "Була", gender: "female")
+
+zubr = FactoryGirl.create(:book, title: "Зубр шукає гніздо")
+vedmid = FactoryGirl.create(:book, title: "Ведмідь не хоче спати")
+
+Work.create!(book: zubr, person_alias: oksana_bula.main_alias, type: text_author_type)
+Work.create!(book: zubr, person_alias: oksana_bula.main_alias, type: illustrator_type)
+Work.create!(book: vedmid, person_alias: oksana_bula.main_alias, type: text_author_type)
+Work.create!(book: vedmid, person_alias: oksana_bula.main_alias, type: illustrator_type)
