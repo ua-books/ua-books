5bd4f805067f3a33223711165b9b01f8a41c33a9
nashbridges <pleasesendmeaspam@gmail.com>
2017-11-18 14:43:23 +0200

    Use Work#title in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ oksana_bula = FactoryGirl.create(:person, :with_alias, first_name: "Оксана
 zubr = FactoryGirl.create(:book, title: "Зубр шукає гніздо")
 vedmid = FactoryGirl.create(:book, title: "Ведмідь не хоче спати")
 
-Work.create!(book: zubr, person_alias: oksana_bula.main_alias, type: text_author_type)
+Work.create!(title: true, book: zubr, person_alias: oksana_bula.main_alias, type: text_author_type)
 Work.create!(book: zubr, person_alias: oksana_bula.main_alias, type: illustrator_type)
-Work.create!(book: vedmid, person_alias: oksana_bula.main_alias, type: text_author_type)
+Work.create!(title: true, book: vedmid, person_alias: oksana_bula.main_alias, type: text_author_type)
 Work.create!(book: vedmid, person_alias: oksana_bula.main_alias, type: illustrator_type)
