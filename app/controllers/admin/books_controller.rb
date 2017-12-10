5f1aeaf7a99e675766581b405928ab4c22526fab
nashbridges <pleasesendmeaspam@gmail.com>
2017-12-10 19:55:43 +0200

    Display `cover` on admin/books#index

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,12 +1,18 @@
 module Admin
   class BooksController < Admin::ApplicationController
-    expose(:index_columns) { %w[id title published_on] }
+    expose(:index_columns) { %w[id cover title published_on] }
     expose(:resource_collection) { Book.order("id desc") }
     expose(:resource, model: "Book")
 
     helper do
       def resource_name(resource)
         resource.title
+      end
+
+      def cover_column(resource)
+        if resource.cover
+          link_to image_tag(resource.cover.thumb("x100").url), resource.cover.url
+        end
       end
 
       def published_on_column(resource)
