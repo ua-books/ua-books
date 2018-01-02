7b1b26cbe62524ce7cb42766c41fb4f29f7dad9b
nashbridges <pleasesendmeaspam@gmail.com>
2018-01-02 22:54:56 +0200

    Stub /about and /helping-us pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For SEO reasons we want books#show to be as short as possible, thus skipping /books prefix
   get "(:slug)/:id" => "books#show", as: :book, constraints: {id: /\d+/}
   get "/" => "home#show", as: :root
+  get "/:path", to: "static#show", as: :static, constraints: {path: /about|helping-us/}
 
   namespace :admin do
     resources :books
