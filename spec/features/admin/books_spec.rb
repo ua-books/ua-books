f2c1215e1ec12313ce429296af100ec04f1daef8
nashbridges <pleasesendmeaspam@gmail.com>
2018-01-08 22:19:53 +0200

    Books form i18n

--- spec/features/admin/books_spec.rb
+++ spec/features/admin/books_spec.rb
@@ -7,27 +7,27 @@ RSpec.describe "Admin::BooksController" do
     expect(page).to have_css :h1, text: %r{^Books / New$}
     expect(page.title).to eq "Books / New | Admin"
 
-    fill_in "Title", with: "Ведмідь не хоче спати"
-    fill_in "Number of pages", with: "30"
-    fill_in "Description md", with: "Опис цієї книги"
-    select_date Date.new(2016, 10, 10), from: "Published on"
-    fill_in "Publisher page url", with: "https://starylev.com.ua/"
-    attach_file "Cover", "public/system/dragonfly/development/oksana-bula-vedmid.jpg"
-    click_on "Create Book"
+    fill_in "Назва", with: "Ведмідь не хоче спати"
+    fill_in "Кількість сторінок", with: "30"
+    fill_in "Опис", with: "Опис цієї книги"
+    select_date Date.new(2016, 10, 10), from: "Дата подання до друку"
+    fill_in "Посилання на книгу на сайті видавця", with: "https://starylev.com.ua/"
+    attach_file "Обкладинка", "public/system/dragonfly/development/oksana-bula-vedmid.jpg"
+    click_on "Додати книгу"
 
     expect(page).to have_css :h1, text: /^Books$/
     expect(page).to have_link "Ведмідь не хоче спати", href: "https://starylev.com.ua/"
     expect(page).to have_content "Oct 2016"
 
     click_on "edit"
-    expect(page).to have_field "Description md", with: "Опис цієї книги"
+    expect(page).to have_field "Опис", with: "Опис цієї книги"
   end
 
   specify "#create with a prefilled form" do
     visit "/admin/books/new?book[title]=#{CGI.escape "Ведмідь"}"
 
-    fill_in "Number of pages", with: "30"
-    click_on "Create Book"
+    fill_in "Кількість сторінок", with: "30"
+    click_on "Додати книгу"
 
     expect(page).to have_css :h1, text: /^Books$/
     expect(page).to have_content "Ведмідь"
@@ -41,7 +41,7 @@ RSpec.describe "Admin::BooksController" do
     expect(page).to have_css :h1, text: %r{^Books / Зубр шукає гніздо / Edit$}
     expect(page.title).to eq "Books / Зубр шукає гніздо / Edit | Admin"
 
-    fill_in "Title", with: "Ведмідь не хоче спати"
+    fill_in "Назва", with: "Ведмідь не хоче спати"
     click_on "Update Book"
 
     expect(page).to have_css :h1, text: /^Books$/
