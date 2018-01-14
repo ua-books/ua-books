d997a6fe62dcd78e6109cbaecd931219cbe18bdf
nashbridges <pleasesendmeaspam@gmail.com>
2018-01-14 19:22:17 +0200

    Column name i18n

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -3,5 +3,9 @@ module Admin
     def index_title
       t("admin.#{controller_name}.index.title", default: controller_name.humanize)
     end
+
+    def column_name(attribute)
+      t("simple_form.labels.#{resource.model_name.singular}.#{attribute}", default: attribute.humanize)
+    end
   end
 end
