7fcc05cfde6599510ce570e21f55994eeff4b7d3
nashbridges <pleasesendmeaspam@gmail.com>
2018-01-14 18:58:22 +0200

    Work type i18n

--- spec/features/admin/work_types_spec.rb
+++ spec/features/admin/work_types_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe "Admin::WorkTypesController" do
     expect(page).to have_css :h1, text: %r{^Типи робіт / Додати$}
     expect(page.title).to eq "Типи робіт / Додати | Admin"
 
-    fill_in "Name feminine", with: "Авторка тексту"
-    fill_in "Name masculine", with: "Автор тексту"
-    click_on "Create Work type"
+    fill_in "Назва (жіночий рід)", with: "Авторка тексту"
+    fill_in "Назва (чоловічий рід)", with: "Автор тексту"
+    click_on "Додати тип робіт"
 
     expect(page).to have_css :h1, text: /^Типи робіт$/
     expect(page).to have_content "Авторка тексту"
@@ -24,7 +24,7 @@ RSpec.describe "Admin::WorkTypesController" do
     expect(page).to have_css :h1, text: %r{^Типи робіт / Ілюстратор / Правити$}
     expect(page.title).to eq "Типи робіт / Ілюстратор / Правити | Admin"
 
-    fill_in "Name masculine", with: "Художник"
+    fill_in "Назва (жіночий рід)", with: "Художник"
     click_on "Зберегти правки"
 
     expect(page).to have_css :h1, text: /^Типи робіт$/
