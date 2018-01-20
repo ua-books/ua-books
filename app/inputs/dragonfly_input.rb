ea52e24f7aeff6967e4d4c170e81e1e6dcbfffee
nashbridges <pleasesendmeaspam@gmail.com>
2018-01-20 15:44:35 +0200

    Add description [ci skip]

--- app/inputs/dragonfly_input.rb
+++ app/inputs/dragonfly_input.rb
@@ -1,3 +1,4 @@
+# Displays uploaded image thumbnail (when present) + file input
 class DragonflyInput < SimpleForm::Inputs::Base
   def input(wrapper_options = nil)
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
