ac31c74c8e3d60c4eac8f708ef94d6dc446ab4de
nashbridges <pleasesendmeaspam@gmail.com>
2018-01-20 15:36:19 +0200

    Add DragonflyInput

---
+++ app/inputs/dragonfly_input.rb
@@ -0,0 +1,22 @@
+class DragonflyInput < SimpleForm::Inputs::Base
+  def input(wrapper_options = nil)
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    model = @builder.object
+    tags = []
+
+    if model.public_send("#{attribute_name}_stored?")
+      attachment = model.public_send(attribute_name)
+    else
+      attachment = model.public_send("retained_#{attribute_name}")
+    end
+
+    if attachment
+      tags << template.image_tag(attachment.thumb("x100").url)
+    end
+
+    tags << @builder.file_field(attribute_name, merged_input_options)
+    tags << @builder.hidden_field("retained_#{attribute_name}")
+    safe_join(tags)
+  end
+end
