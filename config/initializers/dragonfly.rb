aa5d99b6e99deda7814f27c34a0ceebb1e4c4e67
Andrii Malyshko <pleasesendmeaspam@gmail.com>
2020-02-29 12:18:19 +0400

    Allow to upload images in development

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,10 +1,36 @@
+module Dragonfly
+  module DevS3DataStore
+    private
+
+    # https://github.com/markevans/dragonfly-s3_data_store/blob/15ba3f39a/lib/dragonfly/s3_data_store.rb#L126-L128
+    def generate_uid(name)
+      "dev/#{super}"
+    end
+  end
+end
+
 Dragonfly.app.configure do
-  datastore :s3,
-    region: "eu-central-1",
-    bucket_name: "ua-books",
-    storage_headers: {}, # remove `{'x-amz-acl' => 'public-read'}` defaults
-    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+  case Rails.env
+  when "development"
+    datastore :s3,
+      region: "eu-central-1",
+      bucket_name: "ua-books",
+      storage_headers: {}, # remove `{'x-amz-acl' => 'public-read'}` defaults
+      # these credentials are scoped to /dev folder only, so are safe to share in public
+      access_key_id: "AKIA53OKKD65DGWLU4QJ",
+      secret_access_key: "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"
+
+    Dragonfly::S3DataStore.class_eval do
+      prepend Dragonfly::DevS3DataStore
+    end
+  else
+    datastore :s3,
+      region: "eu-central-1",
+      bucket_name: "ua-books",
+      storage_headers: {}, # remove `{'x-amz-acl' => 'public-read'}` defaults
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+  end
 end
 
 Dragonfly.logger = Rails.logger
